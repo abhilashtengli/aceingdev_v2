e5866850e6cd07ad3b2a36f8e91ded54376c5db8
Abhilash <abhilasht0007.t@gmail.com>
2025-09-27 11:55:55 +0530

    Update : Project section.

---
+++ src/Components/ProjectSection.tsx
@@ -0,0 +1,11 @@
+const ProjectSection = () => {
+  return (
+    <>
+      <div className="flex h-screen w-full items-center justify-center border border-black">
+        <h1>Project </h1>
+      </div>
+    </>
+  );
+};
+
+export default ProjectSection;
--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/Components/HeroSection";
+import ProjectSection from "@/Components/ProjectSection";
 import SkillsSection from "@/Components/SkillsSection";
 
 const LandingPage = () => {
   return (
     <div className="mx-auto  h-full w-full">
       <HeroSection />
-      <SkillsSection/>
+      <SkillsSection />
+      <ProjectSection/>
     </div>
   );
 };
