fadd7df17bfb510b55d686955c7c35ca346fa19d
Abhilash <abhilasht0007.t@gmail.com>
2025-10-28 08:31:19 +0530

    Update : MVP UI hero section

--- src/Components/Service/MvpService.tsx
+++ src/Components/Service/MvpService.tsx
@@ -18,11 +18,28 @@ const MvpService = () => {
   }, [delivery.length]);
   return (
     <div className="">
-      <div className="flex w-full flex-col items-center justify-center  border-red-700 px-24 py-12 pt-32 text-center">
-        <div className="ml-12 flex flex-col items-center text-5xl font-bold tracking-wider text-neutral-700">
+      <div className="relative flex w-full flex-col items-center justify-center border-red-700 px-24 py-12 pt-32 text-center">
+        {/* ------------------- */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `
+        linear-gradient(to right, #f3f4f6 1px, transparent 1px),
+        linear-gradient(to bottom, #f3f4f6 1px, transparent 1px)
+      `,
+            backgroundSize: "40px 40px",
+            WebkitMaskImage:
+              "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
+            maskImage:
+              "radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%)",
+          }}
+        />
+        {/* Your Content/Components */}
+        {/* ------------------- */}
+        <div className="relative z-10 ml-12 flex flex-col items-center border-red-400 text-5xl font-bold tracking-wider text-neutral-700">
           <div className="flex items-end justify-center">
             <span>MVP Development for</span>
-            <span className="relative ml-3 inline-block h-13 w-72 overflow-hidden rounded-sm bg-gradient-to-r from-yellow-100 to-red-100">
+            <span className="relative ml-3 inline-block h-13 w-72 overflow-hidden rounded-sm border border-neutral-100 bg-gradient-to-r from-yellow-100 via-white to-red-100">
               <AnimatePresence mode="wait">
                 <motion.span
                   key={index}
@@ -30,7 +47,7 @@ const MvpService = () => {
                   animate={{ y: 0, opacity: 1 }}
                   exit={{ y: -20, opacity: 0 }}
                   transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="absolute top-0 left-0 mask-t-from-70% py-1 pl-3 text-start bg-gradient-to-r from-blue-600 via-cyan-500 to-green-400 bg-clip-text text-transparent"
+                  className="absolute top-0 left-0 bg-gradient-to-r from-blue-600 via-cyan-500 to-green-400 mask-t-from-70% bg-clip-text py-1 pl-3 text-start text-transparent"
                 >
                   {delivery[index]}
                 </motion.span>
@@ -38,15 +55,17 @@ const MvpService = () => {
             </span>
           </div>
         </div>
-
-        {/* <p className="mt-3 text-lg font-semibold tracking-wider text-neutral-700">
-          From Idea to Functional Product Fast, Efficient, Market Ready
-        </p> */}
         <p className="mt-3 max-w-3xl text-xl tracking-wide text-neutral-700">
           We don’t just build MVPs we build market ready products designed to
           validate real demand, attract users, and scale without rebuilding from
           scratch.
         </p>
+        <p className="mt-3 rounded-sm border px-2 py-1 pt-1.5 text-xs tracking-wider text-neutral-800 shadow-sm">
+          From Idea to Functional Product - Fast, Efficient, Market Ready
+        </p>
+      </div>
+      <div className="border border-blue-600">
+        <h1>What You Receive in Our MVP Program </h1>
       </div>
     </div>
   );
